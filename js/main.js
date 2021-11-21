ced315b079c0311a0ee07ba99e0478b62586e701
saidbek04 <atambayevsaid84@gmail.com>
2021-11-21 21:00:34 +0300

    modal js add

--- js/main.js
+++ js/main.js
@@ -8,3 +8,20 @@ modal_button_open.addEventListener("click", () => {
 modal_closer.addEventListener("click", () => {
     modal.style.right = "-100%"
 })
+
+const btns = document.querySelectorAll("[data-target]");
+const close_btn = document.querySelectorAll(".regist-modal--header-close");
+const overlay = document.querySelector("#overlay")
+
+btns.forEach((btn) =>{
+    btn.addEventListener("click" , () => {
+        document.querySelector(btn.dataset.target).classList.add("active");
+        overlay.classList.add("active")
+    });
+});
+close_btn.forEach((btn) =>{
+    btn.addEventListener("click" , () => {
+        document.querySelector(btn.dataset.target).classList.remove("active");
+        overlay.classList.remove ("active")
+    });
+});
